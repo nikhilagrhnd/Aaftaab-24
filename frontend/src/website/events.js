7bd8edd85afbbb084b61446655272b923c755af1
YashBhargava02 <yasbh2002@gmail.com>
2022-09-06 03:17:10 +0530

    Add events page

--- frontend/src/website/events.js
+++ frontend/src/website/events.js
@@ -1,11 +1,24 @@
 import AnimationRevealPage from 'helpers/AnimationRevealPage';
 import React, { Component } from 'react';
+import tw from "twin.macro";
 
+import TabGrid from "components/cards/TabCardGrid-Events.js";
+import Header from 'components/headers/light.js'
+import Footer from "components/footers/MiniCenteredFooter";
 
 function Events() {
+    const HighlightedText = tw.span`bg-primary-500 text-gray-100 px-4 transform -skew-x-12 inline-block`;
     return (
         <AnimationRevealPage>
-            <h1>Events</h1>
+            <Header />
+            <TabGrid
+                heading={
+                <>
+                    Event <HighlightedText>schedule.</HighlightedText>
+                </>
+                }
+            />
+            <Footer />
         </AnimationRevealPage>
     );
 }
