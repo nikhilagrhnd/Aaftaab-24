b209fc939879a921c8f5e81b409102df466d3df7
Jahnab Dutta <76475829+JahnabDutta@users.noreply.github.com>
2022-09-06 03:19:52 +0530

    homepage tentatively done

--- frontend/src/components/headers/light.js
+++ frontend/src/components/headers/light.js
@@ -6,7 +6,7 @@ import { css } from "styled-components/macro"; //eslint-disable-line
 
 import useAnimatedNavToggler from "../../helpers/useAnimatedNavToggler.js";
 
-import logo from "../../images/logo.svg";
+import logo from "../../images/old-logo-symbol.png";
 import { ReactComponent as MenuIcon } from "feather-icons/dist/icons/menu.svg";
 import { ReactComponent as CloseIcon } from "feather-icons/dist/icons/x.svg";
 
@@ -86,7 +86,7 @@ export default ({ roundedHeaderButton = false, logoLink, links, className, colla
   const defaultLogoLink = (
     <LogoLink href="/">
       <img src={logo} alt="logo" />
-      Treact
+      Aaftaab
     </LogoLink>
   );
 
