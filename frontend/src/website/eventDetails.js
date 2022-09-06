f352572cd552acbfd6b00a8b463d27c320b33c83
YashBhargava02 <yasbh2002@gmail.com>
2022-09-06 07:01:25 +0530

    Linked event to event details page

--- frontend/src/website/eventDetails.js
+++ frontend/src/website/eventDetails.js
@@ -1,6 +1,6 @@
 import AnimationRevealPage from 'helpers/AnimationRevealPage';
 import React, { Component } from 'react';
-import { useLocation } from 'react-router-dom';
+import { useLocation, useHistory } from 'react-router-dom';
 import tw from "twin.macro";
 
 import Feature from "components/features/TwoColSingleFeatureWithStats2.js";
@@ -10,14 +10,22 @@ import Footer from "components/footers/Home-Footer";
 function Events() {
     const location = useLocation()
     const card = location.state
+    const history = useHistory()
+
     console.log(location, card)
 
     const HighlightedText = tw.span`bg-primary-500 text-gray-100 px-4 transform -skew-x-12 inline-block`;
     return (
         <AnimationRevealPage>
             <Header />
-            <h1>{card ? card.title : "No details"}</h1>
-            <Feature />
+            {/* <h1>{card ? card.title : history.push("/events")}</h1> */}
+            <Feature 
+                subheading={card.timing}
+                heading={card.title}
+                description={card.description ? card.description : null}
+                imageSrc={card.imageSrc}
+                primaryButtonText="Register Now!"
+            />
             <Footer />
         </AnimationRevealPage>
     );
