5562b3a114a6728d264006d0bf9473a227a4e35b
YashBhargava02 <yasbh2002@gmail.com>
2022-09-14 01:02:34 +0530

    Minor css changes in Hero section

--- frontend/src/components/hero/Hero-Section-Homepage.js
+++ frontend/src/components/hero/Hero-Section-Homepage.js
@@ -23,9 +23,9 @@ import heroImgBL from '../../images/logo-bottom-left.png'
 import heroImgBR from '../../images/logo-bottom-right.png'
 
 const Container = tw.div`relative`;
-const TwoColumn = tw.div`flex flex-col-reverse lg:flex-row md:items-center max-w-screen-xl mx-auto py-5 lg:py-10 md:py-24`;
+const TwoColumn = tw.div`flex flex-col-reverse lg:flex-row md:items-center max-w-screen-xl mx-auto py-5 lg:py-10 md:py-8`;
 const LeftColumn = tw.div`relative lg:w-6/12 lg:pr-12 flex-shrink-0 text-center lg:text-left`;
-const RightColumn = tw.div`relative mt-12 lg:mt-0 flex flex-col justify-center`;
+const RightColumn = tw.div`relative mt-0 mb-12 lg:mt-0 lg:mb-0 flex flex-col justify-center`;
 
 const Heading = tw.h1`font-black text-3xl md:text-5xl leading-snug max-w-3xl`;
 const Paragraph = tw.p`my-5 lg:my-8 text-sm lg:text-base font-semibold text-secondary-600 max-w-lg mx-auto lg:mx-0`;
@@ -100,27 +100,27 @@ export default ({
           </LeftColumn>
           <RightColumn>
             <IllustrationContainer>
-				{/* <img
-					css={imageCss}
-					src={imageSrc}
-					alt="Hero"
-				/> */}
-				<div className="heroLogo">
-					<div className="container">
-						<div className="part row1">
-							<img className="logo2 logo" src={heroImgTL} />
-							<img className="logo1 logo" src={heroImgTR} />
-						</div>
-						<div className="part row2">
-							<img className="logo3 logo" src={heroImgBL} />
-							<img className="logo4 logo" src={heroImgBR} />
-						</div>
-					</div>
-					<div className="heroTitle">
-						<img className="heroTitleImg" css={imageCss} src={heroImgTitle} />
-					</div>
-				</div>
-              	{imageDecoratorBlob && <DecoratorBlob2 />}
+              {/* <img
+                css={imageCss}
+                src={imageSrc}
+                alt="Hero"
+              /> */}
+              <div className="heroLogo">
+                <div className="container">
+                  <div className="part row1">
+                    <img className="logo2 logo" src={heroImgTL} />
+                    <img className="logo1 logo" src={heroImgTR} />
+                  </div>
+                  <div className="part row2">
+                    <img className="logo3 logo" src={heroImgBL} />
+                    <img className="logo4 logo" src={heroImgBR} />
+                  </div>
+                </div>
+                <div className="heroTitle">
+                  <img className="heroTitleImg" css={imageCss} src={heroImgTitle} />
+                </div>
+              </div>
+              {imageDecoratorBlob && <DecoratorBlob2 />}
             </IllustrationContainer>
           </RightColumn>
         </TwoColumn>
