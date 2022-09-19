aaa617114388c0284a53e0115d27b6e7d2532d22
YashBhargava02 <yasbh2002@gmail.com>
2022-09-19 20:34:19 +0530

    Minor changes

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -85,12 +85,8 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'aaftaab_database',
-        'USER': 'postgres',
-        'PASSWORD': 1234,
-        'HOST': 'localhost',
-        'PORT': '5433',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
